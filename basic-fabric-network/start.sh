af55b579f0d0d723d51f07144e2f4ca2c6d01e7c
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-10-15 12:18:56 +0800

    re-generate msp, orderer genesis block and channel artifacts

--- basic-fabric-network/start.sh
+++ basic-fabric-network/start.sh
@@ -31,9 +31,19 @@ sleep ${FABRIC_START_TIMEOUT}
 
 # use cli to create/join channel
 # Create the channel: mychannel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
+            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+            cli peer channel create -o orderer.example.com:7050 \
+                                    -c mychannel \
+                                    -f /etc/hyperledger/configtx/channel.tx
+
 # Join peer0.org1.example.com to the mychannel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
+            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+            cli peer channel join -b mychannel.block
 
 # Join peer1 to mychannel
-docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" \
+            -e "CORE_PEER_LOCALMSPID=Org1MSP" \
+            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+            cli peer channel join -b mychannel.block
