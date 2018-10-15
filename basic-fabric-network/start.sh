b8cc72d6c7fc6be4e6d3fe36c8ab3fad8e5af49e
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-10-15 13:23:12 +0800

    Update peer0 as anchor peer

--- basic-fabric-network/start.sh
+++ basic-fabric-network/start.sh
@@ -35,12 +35,25 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
             -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
             cli peer channel create -o orderer.example.com:7050 \
                                     -c mychannel \
-                                    -f /etc/hyperledger/configtx/channel.tx
+                                    -f /etc/hyperledger/configtx/channel.tx \
+                                    # --tls \
+                                    # --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 # Join peer0.org1.example.com to the mychannel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
             -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
             cli peer channel join -b mychannel.block
+
+# Update peer0 to be an ANCHOR PEER of mychannel in org1
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
+            -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+            -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+            cli peer channel update \
+            -o orderer.example.com:7050 \
+            -c mychannel \
+            -f /etc/hyperledger/configtx/Org1MSPanchors.tx \
+            # --tls \
+            # --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 # Join peer1 to mychannel
 docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" \
