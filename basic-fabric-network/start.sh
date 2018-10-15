4591e065502fe712a2ba4489362020af36ebc7d3
TW-MONEYSQ\evanfang <evanfang@moneysq.com>
2018-10-15 14:02:31 +0800

    enable TLS to orderer

--- basic-fabric-network/start.sh
+++ basic-fabric-network/start.sh
@@ -33,11 +33,12 @@ sleep ${FABRIC_START_TIMEOUT}
 # Create the channel: mychannel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
             -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
+            -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
             cli peer channel create -o orderer.example.com:7050 \
                                     -c mychannel \
                                     -f /etc/hyperledger/configtx/channel.tx \
-                                    # --tls \
-                                    # --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+                                    --tls \
+                                    --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 # Join peer0.org1.example.com to the mychannel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
@@ -52,8 +53,8 @@ docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
             -o orderer.example.com:7050 \
             -c mychannel \
             -f /etc/hyperledger/configtx/Org1MSPanchors.tx \
-            # --tls \
-            # --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+            --tls \
+            --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 # Join peer1 to mychannel
 docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" \
